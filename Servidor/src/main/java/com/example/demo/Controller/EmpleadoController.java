c8e8180c256b9389035c279292aa39278ce2ad14
hectorcaballero21 <hectorlourri21@gmail.com>
2026-02-03 09:05:10 +0100

    update: cambio en EmpleadoController + app.prop

--- Servidor/src/main/java/com/example/demo/Controller/EmpleadoController.java
+++ Servidor/src/main/java/com/example/demo/Controller/EmpleadoController.java
@@ -1,11 +1,11 @@
-package com.example.demo.controller;
+package com.example.demo.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.Entity.EmpleadoEntity;
 import com.example.demo.services.EmpleadoServicio;
 
 @RestController
@@ -14,10 +14,13 @@ public class EmpleadoController {
 	@Autowired
 	private EmpleadoServicio empleadoServicio;
 	
+	//EMPLEADO EN EL QUE SE VA A INICIAR SESIÓN (GUARDA LOS DATOS DEL USUARIO QUE SE BUSQUE EN BASE AL USUARIO Y CONTRASEÑA)
+	EmpleadoEntity empLogIn;
+	
 	//PRUEBA DE CONSULTA EN / PARA QUE SE EJECUTE SI O SI (SIN WEB, EN CONSOLA).
 	@GetMapping("/")
 	public void getLoginPage(Model model) {
-		empleadoServicio.loginEmpleado("spring", "tool");
+		empLogIn = empleadoServicio.loginEmpleado("spring", "tool");
 		
 	}
 	
