70d18f7a3c2460ec0bf1a0a5602747cc869cd351
fermar <josemafdezmabu@gmail.com>
2026-02-13 10:41:07 +0100

    Update testing

--- Servidor/src/test/java/com/example/demo/CarritoDTOTest.java
+++ Servidor/src/test/java/com/example/demo/CarritoDTOTest.java
@@ -1,9 +1,8 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.example.demo;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
